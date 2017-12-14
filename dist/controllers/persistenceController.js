1aae4e391cb444c5230f74a527004608c3256ad7
Kshitiz Arora <kshitizarora@Kshitizs-MacBook-Pro.local>
2017-12-13 23:23:17 -0500

    updated output

--- dist/controllers/persistenceController.js
+++ dist/controllers/persistenceController.js
@@ -1,58 +1,94 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const persistence_1 = require("../models/persistence");
+const persistenceConfig_1 = require("../models/persistenceConfig");
+
 exports.savePersistenceModel = (req, res, next) => {
     console.log(req.body.partnerCode);
-    let partnerCode = req.body.partnerCode;
-    let contactCode = req.body.contactCode;
-    let documentType = req.body.documentType;
-    persistence_1.default.find({ $and: [{ 'documentType': documentType }, { 'contactCode': contactCode }] }).exec((err, result) => {
+    const persistenceOnOff = false;
+    const partnerCode = req.body.partnerCode;
+    const contactCode = req.body.contactCode;
+    const documentType = req.body.documentType;
+    persistenceConfig_1.default.find({ "partnerCode": partnerCode }).exec((err, result) => {
         if (err) {
             return next(err);
         }
         if (result.length > 0) {
             console.log(result);
-            persistence_1.default.update({ $and: [{ 'documentType': documentType }, { 'contactCode': contactCode }] }, {
-                events: req.body.events
-            }, (err, update) => {
-                if (err) {
-                    return next(err);
-                }
-                res.setHeader("Access-Control-Allow-Origin", "*");
-                res.json({ "msg": "Persistence Model updated Successfully" });
-            });
-        }
-        else {
-            const actionModel = new persistence_1.default({
-                partnerCode: req.body.partnerCode,
-                contactCode: req.body.contactCode,
-                documentType: req.body.documentType,
-                dateModified: new Date(),
-                events: req.body.events
-            });
-            actionModel.save((err) => {
-                if (err) {
-                    return next(err);
-                }
-                res.setHeader("Access-Control-Allow-Origin", "*");
-                res.json({ "msg": "Persistence Model Saved Successfully" });
-            });
         }
     });
+    if (persistenceOnOff) {
+        persistence_1.default.find({ $and: [{ "documentType": documentType }, { "contactCode": contactCode }] }).exec((err, result) => {
+            if (err) {
+                return next(err);
+            }
+            if (result.length > 0) {
+                console.log(result);
+                persistence_1.default.update({ $and: [{ "documentType": documentType }, { "contactCode": contactCode }] }, {
+                    events: req.body.events
+                }, (err, update) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    res.json({ "msg": "Persistence Model updated Successfully" });
+                });
+            }
+            else {
+                const actionModel = new persistence_1.default({
+                    partnerCode: req.body.partnerCode,
+                    contactCode: req.body.contactCode,
+                    documentType: req.body.documentType,
+                    dateModified: new Date(),
+                    events: req.body.events
+                });
+                actionModel.save((err) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    res.json({ "msg": "Persistence Model Saved Successfully" });
+                });
+            }
+        });
+    }
 };
 exports.getPersistenceModel = (req, res, next) => {
-    let partnerCode = req.body.partnerCode;
-    let contactCode = req.body.contactCode;
-    let documentType = req.body.documentType;
-    persistence_1.default.find({ $and: [{ 'documentType': documentType },
-            { 'contactCode': contactCode }] }).exec((err, result) => {
+    const persistenceOnOff = false;
+    const partnerCode = req.body.partnerCode;
+    const contactCode = req.body.contactCode;
+    const documentType = req.body.documentType;
+    persistenceConfig_1.default.find({ "partnerCode": partnerCode }).exec((err, result) => {
         if (err) {
             return next(err);
         }
-        if (result) {
-            console.log(result);
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            return res.json(result);
+        if (result.length > 0) {
+            if (result[0].persistenceFlag) {
+                persistence_1.default.find({ $and: [{ "documentType": documentType },
+                        { "contactCode": contactCode }] }).exec((err, result) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    if (result) {
+                        console.log(result);
+                        res.setHeader("Access-Control-Allow-Origin", "*");
+                        return res.json(result);
+                    }
+                });
+            }
+            else {
+                return "Persistence is off";
+            }
+        }
+        else {
+            const partnerConfig = new persistenceConfig_1.default({
+                partnerCode: req.body.partnerCode,
+                persistenceFlag: true
+            });
+            partnerConfig.save((err) => {
+                if (err) {
+                    return next(err);
+                }
+                res.json({ "msg": "Persistence config Saved Successfully" });
+            });
         }
     });
 };
--- src/controllers/persistenceController.ts
+++ src/controllers/persistenceController.ts
@@ -1,57 +1,85 @@
 import { default as persistence, persistenceModel } from "../models/persistence";
+import { default as persistenceConfig, persistenceConfigModel } from "../models/persistenceConfig";
 import { Request, Response, NextFunction } from "express";
-
-
 
 export let savePersistenceModel = (req: Request, res: Response, next: NextFunction) => {
     console.log(req.body.partnerCode);
-    let partnerCode = req.body.partnerCode;
-    let contactCode = req.body.contactCode;
-    let documentType = req.body.documentType;
-    persistence.find({ $and: [ { 'documentType': documentType} ,{'contactCode' : contactCode}] }).exec((err,result)=>{
-    if(err){
-        return next(err);
-    }
-    if(result.length > 0){
-        console.log(result);
-        persistence.update({ $and: [ { 'documentType': documentType} ,{'contactCode' : contactCode}] },{
-            events : req.body.events
-        },(err,update)=>{
-            if (err) { return next(err); }
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.json({ "msg": "Persistence Model updated Successfully" });
+    const persistenceOnOff =  false;
+    const partnerCode = req.body.partnerCode;
+    const contactCode = req.body.contactCode;
+    const documentType = req.body.documentType;
+    persistenceConfig.find({"partnerCode" : partnerCode}).exec((err, result) => {
+        if (err) {
+            return next(err);
+        }
+        if (result.length > 0) {
+        if (result[0].persistenceFlag) {
+            persistence.find({ $and: [ { "documentType": documentType} , {"contactCode" : contactCode}] }).exec((err, result) => {
+            if (err) {
+                return next(err);
+            }
+            if (result.length > 0) {
+                console.log(result);
+                persistence.update({ $and: [ { "documentType": documentType} , {"contactCode" : contactCode}] }, {
+                    events : req.body.events
+                }, (err, update) => {
+                    if (err) { return next(err); }
+                    res.json({ "msg": "Persistence Model updated Successfully" });
+                });
+            }
+            else {
+                const actionModel = new persistence({
+                    partnerCode: req.body.partnerCode,
+                    contactCode: req.body.contactCode,
+                    documentType: req.body.documentType,
+                    dateModified : new Date(),
+                    events : req.body.events
+                });
+                actionModel.save((err) => {
+                    if (err) { return next(err); }
+                res.json({ "msg": "Persistence Model Saved Successfully" });
+                });
+            }
         });
     }
-    else{
-        const actionModel = new persistence({
-            partnerCode: req.body.partnerCode,
-            contactCode: req.body.contactCode,
-            documentType: req.body.documentType,
-            dateModified : new Date(),
-            events : req.body.events
-          });
-          actionModel.save((err) => {
-            if (err) { return next(err); }
-            res.setHeader("Access-Control-Allow-Origin", "*");
-          res.json({ "msg": "Persistence Model Saved Successfully" });
-          });
+    else {
+        res.json({"msg": "Persistence is off"});
     }
+}
+else {
+    res.json({"msg": "No Configuration found for this Buyer partner code"});
+}
 });
 };
 export let getPersistenceModel = (req: Request, res: Response, next: NextFunction) => {
-    let partnerCode = req.body.partnerCode;
-    let contactCode = req.body.contactCode;
-    let documentType = req.body.documentType;
-
-    persistence.find({ $and: [ { 'documentType': documentType} ,
-        {'contactCode' : contactCode}] }).exec((err,result)=>{
-        if(err){
+    const persistenceOnOff = false;
+    const partnerCode = req.body.partnerCode;
+    const contactCode = req.body.contactCode;
+    const documentType = req.body.documentType;
+    persistenceConfig.find({"partnerCode" : partnerCode}).exec((err, result) => {
+        if (err) {
             return next(err);
         }
-        if(result){
-            console.log(result);
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            return res.json(result);
+        if (result.length > 0) {
+        if (result[0].persistenceFlag) {
+            persistence.find({ $and: [ { "documentType": documentType} ,
+                {"contactCode" : contactCode}] }).exec((err, result) => {
+                if (err) {
+                    return next(err);
+                }
+                if (result) {
+                    console.log(result);
+                    res.setHeader("Access-Control-Allow-Origin": "*");
+                    return res.json(result);
+                }
+            });
         }
-    });
+        else {
+            res.json({"msg": "Persistence is off"});
+        }
+    }
+    else {
+        res.json({"msg": "No Configuration found for this Buyer partner code"});
+    }
+});
 };
