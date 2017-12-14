03312d18fe6d0b51b31d6cd87b4f060cb78d7db8
Kshitiz Arora <kshitizarora@Kshitizs-MacBook-Pro.local>
2017-12-13 23:33:01 -0500

    updated output

--- dist/controllers/persistenceController.js
+++ dist/controllers/persistenceController.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const persistence_1 = require("../models/persistence");
 const persistenceConfig_1 = require("../models/persistenceConfig");
-
 exports.savePersistenceModel = (req, res, next) => {
     console.log(req.body.partnerCode);
     const persistenceOnOff = false;
@@ -14,42 +13,47 @@ exports.savePersistenceModel = (req, res, next) => {
             return next(err);
         }
         if (result.length > 0) {
-            console.log(result);
-        }
-    });
-    if (persistenceOnOff) {
-        persistence_1.default.find({ $and: [{ "documentType": documentType }, { "contactCode": contactCode }] }).exec((err, result) => {
-            if (err) {
-                return next(err);
-            }
-            if (result.length > 0) {
-                console.log(result);
-                persistence_1.default.update({ $and: [{ "documentType": documentType }, { "contactCode": contactCode }] }, {
-                    events: req.body.events
-                }, (err, update) => {
+            if (result[0].persistenceFlag) {
+                persistence_1.default.find({ $and: [{ "documentType": documentType }, { "contactCode": contactCode }] }).exec((err, result) => {
                     if (err) {
                         return next(err);
                     }
-                    res.json({ "msg": "Persistence Model updated Successfully" });
+                    if (result.length > 0) {
+                        console.log(result);
+                        persistence_1.default.update({ $and: [{ "documentType": documentType }, { "contactCode": contactCode }] }, {
+                            events: req.body.events
+                        }, (err, update) => {
+                            if (err) {
+                                return next(err);
+                            }
+                            res.json({ "msg": "Persistence Model updated Successfully" });
+                        });
+                    }
+                    else {
+                        const actionModel = new persistence_1.default({
+                            partnerCode: req.body.partnerCode,
+                            contactCode: req.body.contactCode,
+                            documentType: req.body.documentType,
+                            dateModified: new Date(),
+                            events: req.body.events
+                        });
+                        actionModel.save((err) => {
+                            if (err) {
+                                return next(err);
+                            }
+                            res.json({ "msg": "Persistence Model Saved Successfully" });
+                        });
+                    }
                 });
             }
             else {
-                const actionModel = new persistence_1.default({
-                    partnerCode: req.body.partnerCode,
-                    contactCode: req.body.contactCode,
-                    documentType: req.body.documentType,
-                    dateModified: new Date(),
-                    events: req.body.events
-                });
-                actionModel.save((err) => {
-                    if (err) {
-                        return next(err);
-                    }
-                    res.json({ "msg": "Persistence Model Saved Successfully" });
-                });
+                res.json({ "msg": "Persistence is off" });
             }
-        });
-    }
+        }
+        else {
+            res.json({ "msg": "No Configuration found for this Buyer partner code" });
+        }
+    });
 };
 exports.getPersistenceModel = (req, res, next) => {
     const persistenceOnOff = false;
@@ -69,26 +73,16 @@ exports.getPersistenceModel = (req, res, next) => {
                     }
                     if (result) {
                         console.log(result);
-                        res.setHeader("Access-Control-Allow-Origin", "*");
                         return res.json(result);
                     }
                 });
             }
             else {
-                return "Persistence is off";
+                res.json({ "msg": "Persistence is off" });
             }
         }
         else {
-            const partnerConfig = new persistenceConfig_1.default({
-                partnerCode: req.body.partnerCode,
-                persistenceFlag: true
-            });
-            partnerConfig.save((err) => {
-                if (err) {
-                    return next(err);
-                }
-                res.json({ "msg": "Persistence config Saved Successfully" });
-            });
+            res.json({ "msg": "No Configuration found for this Buyer partner code" });
         }
     });
 };
